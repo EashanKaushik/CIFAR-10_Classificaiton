950e39d4631d847b2fb0834afcd3c1da0e395e12
EashanKaushik <eashank16@gmail.com>
2021-07-21 18:57:10 +0530

    deployment complete

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ app = Flask(__name__)
 app.add_url_rule('/predict', 'predict', views.predict, methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
